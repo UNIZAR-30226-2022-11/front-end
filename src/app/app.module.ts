0e64240ef30b88b6a18dae2c3c5d95d91b4a6594
Ernesting <ernesto.6.ds@gmail.com>
2022-03-16 11:07:43 +0100

    1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MiComponente } from './components/mi-componente/mi-componente.component';
-import { PeliculasComponent } from './components/peliculas/peliculas.component';
-import { PruebasComponent } from './components/pruebas/pruebas.component';
+import { JuegoComponent } from './components/juego/juego.component';
 import { LoginComponent } from './components/login/login.component';
-import { UserComponent } from './components/user/user.component';
-import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+
+const rutas: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
+    path: 'juego',
+    component: JuegoComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponente,
-    PeliculasComponent,
-    PruebasComponent,
+    JuegoComponent,
     LoginComponent,
-    UserComponent
-
+    HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule,
+    RouterModule.forRoot(rutas, {
+      paramsInheritanceStrategy:'always'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
